f2596e2d3152af91c38d26d7ec5260fec2a8d378
wissaoui <walid.issaoui@ext.caudalie.com>
2026-04-09 23:56:08 +0200

    ci: update kill all containers

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var app = builder.Build();
 
 app.MapGet("/api/hello", () => new {
 
-    message = "DevOps v4 — conteneurisé Docker CI/CD !",
+    message = "DevOps v5 — conteneurisé Docker CI/CD !",
 
     timestamp = DateTime.UtcNow,
 
